55967f2efe2e1ca14118dfe3b9057764c6110044
David Patiño <david.andrespr5@gmail.com>
2022-04-20 16:02:41 -0500

    4. Agregando Archivo index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander= require("./app/missionCommander")
+
+const mc1 = new MissionCommander('Ferndanda Ochoa')
+const mc2 = new MissionCommander('Rodrigo Martinez')
+const mc3 = new MissionCommander('Carlo Gilmar')
+
+console.log(mc1.name)
+console.log(mc2.name)
+console.log(mc3.name)
